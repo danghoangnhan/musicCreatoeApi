ee8a09983cbffd8667ac2e397eceb048d7a09da9
Daniel Du <57485952+danghoangnhan@users.noreply.github.com>
2020-11-22 13:00:12 +0800

    delete description

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,9 @@
 'use strict';
 module.exports = function(app) {
-  // let productsCtrl = require('./../controllers/ProductsController');
-  // let accountsCtrl = require('./../controllers/AccountController');
-  // let vehiclesCtrl = require('./../controllers/VehiclesController');
-  // let commandCtrl = require('./../controllers/CommandController');
-
-  // todoList Routes
-  // app.route('/products')
-  //   .get(productsCtrl.get)
-  //   .post(productsCtrl.store);
 
 
-  //   app.route('/account')
-  //   .get(accountsCtrl.get)
-  //   .put(accountsCtrl.update)
-  //   .delete(accountsCtrl.delete);
-
-  //   app.route('/command')
-  //   .get(commandCtrl.get)
-  //   .put(commandCtrl.update)
-  //   .delete(commandCtrl.delete);
-
-  //   app.route('vehicle')
-  //   .get(vehiclesCtrl.get)
-  //   .put(vehiclesCtrl.update)
-  //   .delete(vehiclesCtrl.delete);
 };
-/**
- * Created by trungquandev.com's author on 16/10/2019.
- * src/routes/api.js
- */
+
 const express = require("express");
 const router = express.Router();
 const AuthMiddleWare = require("../middleware/AuthMiddleware");
