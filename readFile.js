b6395ba0be66f9418216ca24cfc391f650081dca
đặng hoàng nhân <danghoangnhan.1@gmail.com>
2020-11-24 15:55:24 +0800

    add readFile

--- readFile.js
+++ readFile.js
@@ -1,6 +1,8 @@
+const path = require('path');
 var fs = require('fs');
 var file = require('file-system');
 var files = fs.readdirSync('E:/readFile');
-files.forEach(element => {
-    
-});
+for(var i=0;i<files.length;i++){ 
+    var sql = "INSERT INTO `tune` VALUES (" + i + ",'" + path.parse(files[i]).name +"', '140.136.151.130:80/music/tune/"+files[i]+"')";
+    console.log(sql);
+};
