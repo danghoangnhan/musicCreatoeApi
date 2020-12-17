a0f0a2cd6e6b492052f49e879e6b446c913ccef2
Daniel Du <57485952+danghoangnhan@users.noreply.github.com>
2020-12-18 00:09:04 +0800

    Update readFile.js

--- readFile.js
+++ readFile.js
@@ -4,7 +4,8 @@ var fs = require('fs');
 var file = require('file-system');
 var files = fs.readdirSync('E:/musicCreatoeApi/music/song');
 for(var i=0;i<files.length;i++){ 
-    var sql = "INSERT INTO `song` VALUES (" + i + ",'" + path.parse(files[i]).name +"', '140.136.151.130:80/music/song/"+files[i]+"')";
+    //var sql = "INSERT INTO `song` VALUES (" + i + ",'" + path.parse(files[i]).name +"', '140.136.151.130:80/music/song/"+files[i]+"')";
+    var sql = "INSERT INTO `song` (songName ,path,tune_set) VALUES ('" + path.parse(files[i]).name +"', '140.136.151.130:80/music/song/"+files[i]+"',NULL)";
     db.query(sql, function (error, results, fields) {
         if (error) {
             console.log('[UPDATE ERROR] - ', error.message);
